e3f679b59a15533c44d10193f999d6919cbfa9d9
Henrique Andrade <henrique.ccda@gmail.com>
2023-06-16 19:55:04 -0300

    Criando Testes e o CI.
    close #1 #4

--- backend/api/services.py
+++ backend/api/services.py
@@ -14,9 +14,9 @@ async def insert_fav(book, db: MongoClient):
     db.favoritos.insert_one(book)
 
 
-async def delete_fav(id: str, db: MongoClient):
-    db.favoritos.delete_one({"_id": ObjectId(id)})
+async def delete_fav(id_: str, db: MongoClient):
+    db.favoritos.delete_one({"_id": ObjectId(id_)})
 
 
-async def get_book_by_id(id: str, db: MongoClient):
-    return db.livros.find_one({"_id": ObjectId(id)})
+async def get_book_by_id(id_: str, db: MongoClient):
+    return db.livros.find_one({"_id": ObjectId(id_)})
